6144efd1ce43582353c2715ff0e1612df01318c6
Peter Wood <peter@alastria.net>
2020-02-09 16:36:20 +0000

    Add bind and unbind operations to for nodes.

--- node_operations.go
+++ node_operations.go
@@ -8,4 +8,7 @@ type NodeOperations interface {
 	QueryNodeDescription(ctx context.Context, networkAddress NetworkAddress) (NodeDescription, error)
 	QueryNodeEndpoints(ctx context.Context, networkAddress NetworkAddress) ([]byte, error)
 	QueryNodeEndpointDescription(ctx context.Context, networkAddress NetworkAddress, endpoint byte) (EndpointDescription, error)
+
+	BindToNode(ctx context.Context, networkAddress NetworkAddress, sourceAddress IEEEAddress, sourceEndpoint byte, destinationAddress IEEEAddress, destinationEndpoint byte, cluster ZCLClusterID) error
+	UnbindFromNode(ctx context.Context, networkAddress NetworkAddress, sourceAddress IEEEAddress, sourceEndpoint byte, destinationAddress IEEEAddress, destinationEndpoint byte, cluster ZCLClusterID) error
 }
